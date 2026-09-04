e31c361661d5786d9cd278604c3568cd34f29eba
doomfuggler <doomfuggler@gmail.com>
2026-09-04 19:58:38 +0800

    Add obfuscated script c9bf1619

---
+++ scripts/c9bf1619.lua
@@ -0,0 +1,8 @@
+-- Obfuscated by Luau Shield
+local _grHOOMsm="68"
+local _ExpFqAvE=""
+for _TUN=1,#_grHOOMsm,2 do
+  _wATXT=tonumber(_grHOOMsm:sub(_TUN,_TUN+1),16)
+  _ExpFqAvE=_ExpFqAvE..string.char(_wATXT)
+end
+loadstring(_ExpFqAvE)()
