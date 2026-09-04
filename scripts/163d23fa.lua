8b63826d07cee629ff105a1f371cb946a73c884b
doomfuggler <doomfuggler@gmail.com>
2026-09-04 20:28:54 +0800

    Delete scripts/163d23fa.lua

--- scripts/163d23fa.lua
+++
@@ -1,8 +0,0 @@
--- Obfuscated by Luau Shield
-local _gFpYbhcY="6869"
-local _WtOYFnAR=""
-for _VzE=1,#_gFpYbhcY,2 do
-  _gpXmX=tonumber(_gFpYbhcY:sub(_VzE,_VzE+1),16)
-  _WtOYFnAR=_WtOYFnAR..string.char(_gpXmX)
-end
-loadstring(_WtOYFnAR)()
