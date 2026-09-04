a955b1a8e08d5ad7368bde865e1f1bf8cbc5ea45
doomfuggler <doomfuggler@gmail.com>
2026-09-04 17:51:18 +0800

    Add obfuscated script 51eeeb08

---
+++ scripts/51eeeb08.lua
@@ -0,0 +1,8 @@
+-- Obfuscated by Luau Shield
+local _RAGEJIqs="6869"
+local _mwhbjxlq=""
+for _pYf=1,#_RAGEJIqs,2 do
+  _JxqLy=tonumber(_RAGEJIqs:sub(_pYf,_pYf+1),16)
+  _mwhbjxlq=_mwhbjxlq..string.char(_JxqLy)
+end
+loadstring(_mwhbjxlq)()
